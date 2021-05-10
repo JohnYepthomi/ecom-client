010b87211d407bbf6b7bb9d584dc50bc8c9ed9c9
JohnYepthomi <55916185+JohnYepthomi@users.noreply.github.com>
2021-05-10 23:09:16 +0530

    Update Firebase.js

--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABSE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    apiKey: "AIzaSyDYcfBOw3upax9SSV_0Ho148QT-55TrqW4",
+    authDomain: "ecom-test-61fc1.firebaseapp.com",
+    databaseURL: "https://ecom-test-61fc1.firebaseio.com",
+    projectId: "ecom-test-61fc1",
+    storageBucket: "ecom-test-61fc1.appspot.com",
+    messagingSenderId: "662690549399",
+    appId: "1:662690549399:web:c85a62cedabc02dfe923d2"
   });
 
   export const auth = app.auth();
